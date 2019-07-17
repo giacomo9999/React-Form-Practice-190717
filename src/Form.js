4eed191b63aa22fdc03ef6c58e1d22589028458e
James Gary <gmarchesi888@gmail.com>
2019-07-17 09:55:15 -0400

    Building out Form component.

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.initialState = {
+      name: "",
+      species: "",
+      color: ""
+    };
+    this.state = this.initialState;
+  }
   render() {
     return <h2>I Am The Form</h2>;
   }
