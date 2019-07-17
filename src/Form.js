606f191332dca0dbd4e89c642aef7c9aaa5a8a48
James Gary <gmarchesi888@gmail.com>
2019-07-17 11:48:35 -0400

    Styled the form.

--- src/Form.js
+++ src/Form.js
@@ -10,8 +10,43 @@ class Form extends Component {
     };
     this.state = this.initialState;
   }
+
+  handleChange = e => {
+    const { varName, value } = e.target;
+    this.setState({ [varName]: value });
+  };
+
   render() {
-    return <h2>I Am The Form</h2>;
+    const { name, species, color } = this.state;
+
+    return (
+      <div>
+        <h2>I Am The Form</h2>
+        <form>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleChange}
+          />
+          <label>Species</label>
+          <input
+            type="text"
+            name="species"
+            value={species}
+            onChange={this.handleChange}
+          />
+          <label>Color</label>
+          <input
+            type="text"
+            name="color"
+            value={color}
+            onChange={this.handleChange}
+          />
+        </form>
+      </div>
+    );
   }
 }
 
